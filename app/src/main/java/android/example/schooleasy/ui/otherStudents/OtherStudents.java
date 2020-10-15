8d4436754db5dd33ad152feead26df19e1e18bab
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-10-16 02:23:49 +0530

    initial commit

---
+++ app/src/main/java/android/example/schooleasy/ui/otherStudents/OtherStudents.java
@@ -0,0 +1,20 @@
+package android.example.schooleasy.ui.otherStudents;
+
+import android.example.schooleasy.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OtherStudents extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_otherfragment,container,false);
+        return root;
+    }
+}
---
+++ app/src/main/java/android/example/schooleasy/ui/teacher/Teacher.java
@@ -0,0 +1,20 @@
+package android.example.schooleasy.ui.teacher;
+
+import android.example.schooleasy.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Teacher extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_teacher,container,false);
+        return root;
+    }
+}
